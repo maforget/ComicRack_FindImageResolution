2a394beb0cda026981f4294f75702c19d8eb9eca
maforget <11904426+maforget@users.noreply.github.com>
2023-01-27 14:39:08 -0500

    Changed back to an exe

---
+++ FindImageResolution/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindImageResolution
+{
+    internal static partial class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Dialog.frmConfig());
+        }
+    }
+}
+
