c2445b579cd88edaebbf41c3cff8e9343377987f
maforget <11904426+maforget@users.noreply.github.com>
2023-02-01 21:12:03 -0500

    Split Fast & Slow pages processing.

--- FindImageResolution/BookImageResolution.cs
+++ FindImageResolution/BookImageResolution.cs
@@ -9,7 +9,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using FindImageResolutionNET.ComicRack;
 using FindImageResolutionNET.Tools;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace FindImageResolutionNET
 {
@@ -32,22 +31,43 @@ namespace FindImageResolutionNET
             List<ImageResolutionEventArgs> resolutions = new List<ImageResolutionEventArgs>();
             Token.ThrowIfCancellationRequested();
 
+            resolutions.AddRange(CheckFastPages(book));
+            resolutions.AddRange(CheckSlowPages(book));
+
+            var most = resolutions.GroupBy(x => x.Width).OrderByDescending(g => g.Count()).SelectMany(i => i).FirstOrDefault();
+            SimpleLogger.Info($"Most occurring dimension is: {most.Width} X {most.Height}");
+            OnResolutionFound(most);
+        }
+
+        private IEnumerable<ImageResolutionEventArgs> CheckFastPages(Book book)
+        {
             var pages = book.GetPageList();
             var setting = Setting.Config.ReadUserFromFile();
             bool forceRecheck = setting.ForceRecheck;
             var fastPages = forceRecheck ? Enumerable.Empty<ComicPageInfo>() : pages.Where(p => p?.ImageHeight > 0 || p?.ImageHeight > 0);
-            var slowPages = forceRecheck ? pages : pages.Where(p => p?.ImageHeight == 0 || p?.ImageHeight == 0);
-            int percentageToCheck = setting.PercentageOfSlowInspection;
-            int numberOfPagesToCheck = (int)(percentageToCheck / 100.0 * slowPages.Count());
-            int minNumberOfPagesToCheck = forceRecheck && slowPages.Count() >= 2 && numberOfPagesToCheck <= 2 ? 2 : numberOfPagesToCheck;
-            SimpleLogger.Info($"Number of Pages: {fastPages.Count()} Fast & {slowPages.Count()} Slow // Inspecting {minNumberOfPagesToCheck + fastPages.Count()} Total ({minNumberOfPagesToCheck} Slow - {percentageToCheck}%)");
+            SimpleLogger.Info($"Number of Fast pages: {fastPages.Count()} // Total: {pages.Count()}");
 
+            List<ImageResolutionEventArgs> resolutions = new List<ImageResolutionEventArgs>();
             foreach (var page in fastPages)
             {
                 resolutions.Add(new ImageResolutionEventArgs(page.ImageWidthAsText, page.ImageHeightAsText));
                 SimpleLogger.Debug($"Read page {page.ImageIndex + 1} from cache. {page.ImageWidthAsText} X {page.ImageHeightAsText}");
             }
+            return resolutions;
+        }
 
+        private IEnumerable<ImageResolutionEventArgs> CheckSlowPages(Book book)
+        {
+            var pages = book.GetPageList();
+            var setting = Setting.Config.ReadUserFromFile();
+            bool forceRecheck = setting.ForceRecheck;
+            int percentageToCheck = setting.PercentageOfSlowInspection;
+            var slowPages = forceRecheck ? pages : pages.Where(p => p?.ImageHeight == 0 || p?.ImageHeight == 0);
+            int numberOfPagesToCheck = (int)(percentageToCheck / 100.0 * slowPages.Count());
+            int minNumberOfPagesToCheck = forceRecheck && slowPages.Count() >= 2 && numberOfPagesToCheck <= 2 ? 2 : numberOfPagesToCheck;
+            SimpleLogger.Info($"Number of Slow pages: {slowPages.Count()} (Inspecting {minNumberOfPagesToCheck} - {percentageToCheck}%) // Total: {pages.Count()}");
+
+            List<ImageResolutionEventArgs> resolutions = new List<ImageResolutionEventArgs>();
             int pageIndex = 0;
             foreach (var page in slowPages)
             {
@@ -67,17 +87,14 @@ namespace FindImageResolutionNET
                 }
                 pageIndex++;
             }
-
-            var most = resolutions.GroupBy(x => x.Width).OrderByDescending(g => g.Count()).SelectMany(i => i).FirstOrDefault();
-            SimpleLogger.Info($"Most occurring dimension is: {most.Width} X {most.Height}");
-            OnResolutionFound(most);
+            return resolutions;
         }
 
         protected virtual void OnResolutionFound(ImageResolutionEventArgs e) => ResolutionFound?.Invoke(this, e);
     }
 
     public class ImageResolutionEventArgs : EventArgs
-    {
+    { 
         public string Width { get; }
         public string Height { get; }
 
